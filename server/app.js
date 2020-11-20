7aa87c515a68d6de94dab543eb869ee52807314d
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-11-20 19:37:20 +0700

    Development(mriskyn#2) set up local mongodb with mongoose set up model"s Scheme

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
+require("./config");
 const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 3300; // Biar beda sama react
 const routes = require("./routes");
 
+const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
